6eeecdf5e608d7e0185b0ccfd785d07964b75420
TheRealBasel <alanaziibasel@gmail.com>
2021-10-12 10:33:39 +0300

    Finish Warmup

--- warmup.js
+++ warmup.js
@@ -1,26 +1,25 @@
 let booksList = [];
 
-function addNewBook ( name, url ){
-    booksList.push([name,url]);
+function addNewBook ( bookName, url ){
+    booksList.push([bookName,url]);
     let bookListDOM = document.getElementById("books");
     let bookItem = document.createElement("li");
-    bookItem.id = name;
+    bookItem.id = bookName;
     bookItem.style = "display: flex; vertical-align: middle; margin: 10px 0;"
     bookItem.innerHTML = `
-    <img src="${url}"></img>    
+    <img src="${url}" width="250" height="250"></img>    
     <div style="display: -ms-flexbox; display: -webkit-flex; display: flex; -ms-flex-align: center; -webkit-align-items: center; -webkit-box-align: center; align-items: center;"> 
-        <h3 style="margin-left:30px;">${name}</h3>   
-        <button style="margin-left:30px;" onclick="removeBook(${name})">Delete</button>
-    </div>
-    `;
+        <h3 style="margin-left:30px;">${bookName}</h3>   
+        <button style="margin-left:30px;" onclick="removeBook(${bookName},this)">Delete</button>
+    </div>`;
 
     bookListDOM.appendChild(bookItem);
 }
 
-function removeBook (name){
-    const found = booksList.findIndex(element => element[0] == name);
+function removeBook (bookName,liElement){
+    const found = booksList.findIndex(element => element[0] == bookName);
     booksList[found] = null;
-    name.remove()
+    liElement.parentNode.parentNode.remove();
 }
 
 function onAddClick (){
